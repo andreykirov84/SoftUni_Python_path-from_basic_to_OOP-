2fba2f26f61ad9f77441082aa5345a038eed9263
andreykirov84 <andrey_kirov@mail.bg>
2021-04-28 18:09:22 +0300

    Add solutions to some tasks.

---
+++ 01_Basic_syntax_conditional_statement_and_loops/U_4_Double_Char.py
@@ -0,0 +1,3 @@
+string = input()
+for i in string:
+    print(f'{i}{i}', end='')
---
+++ 01_Basic_syntax_conditional_statement_and_loops/U_9_Easter_Cozonacs.py
@@ -0,0 +1,5 @@
+budget = float(input())
+price_1kg_flour = float(input())
+price_1_egg = 0.75 * price_1kg_flour / 12
+price_1l_milk = 1, 25 * price_1kg_flour
+price_kozunac = 1 *
