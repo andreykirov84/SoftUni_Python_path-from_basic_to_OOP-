b0803cecb7177558fcf7a80b74b65cadad56f3d3
andreykirov84 <andrey_kirov@mail.bg>
2021-05-05 17:40:33 +0300

    more solutions

--- 02._Data_Types_and_Variables/02_Centuries_to_Minutes.py
+++ 02._Data_Types_and_Variables/02_Centuries_to_Minutes.py
@@ -1,6 +1,11 @@
 centuries = int(input())
 years = centuries * 100
-days = round(years * 365.2422)
+days = int(years * 365.2422)
 hours = days * 24
 minutes = hours * 60
 print(f'{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes')
+
+# days2 = round(years * 365.2422)
+# hours2 = days2 * 24
+# minutes2 = hours2 * 60
+# print(f'{centuries} centuri3es = {years} years = {days2} days = {hours2} hours = {minutes2} minutes')
